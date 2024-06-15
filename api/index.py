532474b21a875ed8f85846ce0aedb2948d75cbd6
BD <32562850+jasminebd@users.noreply.github.com>
2024-06-15 19:38:38 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,9 @@
 from sanic import Sanic
 from sanic.response import json
-app = Sanic("__name__")
+app = Sanic("app")
  
  
 @app.route('/')
 @app.route('/<path:path>')
 async def index(request, path=""):
     return json({'hello': path})
-
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3000)
